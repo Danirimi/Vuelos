143d25c12bb4f657e7b93137199aa336d9aa1882
Danirimi <danirimi222007@gmail.com>
2025-07-29 13:41:52 -0600

    empieza la aventura

---
+++ vuelos/src/vuelos/RegistroVuelos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vuelos;
+
+/**
+ *
+ * @author danir
+ */
+public class RegistroVuelos {
+    public String destino; // lugar de destino
+    public int Cpasajeros; // cantidad de pasajeros maximos
+    public String Origen;// lugar de donde sale el vuelo
+    public String FSalida;//fecha de salida
+    public String aereolinia;// aereolinia donde pertecence el vuelo
+}
