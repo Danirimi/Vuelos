d5bc52256861d287c43ecf2fe7b130bb0f4addf5
Danirimi <danirimi222007@gmail.com>
2025-07-31 19:29:33 -0600

    hola
    hola

--- vuelos/src/vuelos/Reserva.java
+++ vuelos/src/vuelos/Reserva.java
@@ -8,6 +8,61 @@ package vuelos;
  *
  * @author frank
  */
-public class Reserva {
+public class Reserva extends Usuarios{
+    public String asiento;
+    public int Nvuelo;
+
+    public Reserva(String asiento, int Nvuelo, String UNombre, String UCorreo, int edad) {
+        super(UNombre, UCorreo, edad);
+        this.asiento = asiento;
+        this.Nvuelo = Nvuelo;
+    }
+
+ 
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public int getNvuelo() {
+        return Nvuelo;
+    }
+
+    public void setNvuelo(int Nvuelo) {
+        this.Nvuelo = Nvuelo;
+    }
+
+    public String getUNombre() {
+        return UNombre;
+    }
+
+    public void setUNombre(String UNombre) {
+        this.UNombre = UNombre;
+    }
+
+    public String getUCorreo() {
+        return UCorreo;
+    }
+
+    public void setUCorreo(String UCorreo) {
+        this.UCorreo = UCorreo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "asiento=" + asiento + ", Nvuelo=" + Nvuelo + '}';
+    }
+    
     
 }
